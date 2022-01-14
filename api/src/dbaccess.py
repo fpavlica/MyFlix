27aa602139f67a47e0da4aeb39aafab0da2e2a36
fpavlica <frpavlica@gmail.com>
2022-01-14 01:50:10 +0000

    basic db connection check

--- api/src/dbaccess.py
+++ api/src/dbaccess.py
@@ -1,5 +1,6 @@
 from flask.blueprints import Blueprint
-from flask import jsonify
+from flask import json, jsonify, request, Response
+import pymongo
 
 dbapi = Blueprint("dbaccess", __name__)
 
@@ -42,3 +43,33 @@ def get_film_link(film_name):
     # fname = film.v_fname
     return jsonify({"url": host+fname})
 
+@dbapi.route("/add_film", methods=['POST'])
+def add_film():
+    data = request.form
+    jsondata = request.json
+    print(f'Request to add film: {data}')
+    print(f'in json: {jsondata}')
+
+    mongoclient = pymongo.MongoClient("mongodb://35.240.101.156:80")
+    mydb = mongoclient["filmsdb"]
+    filmscol = mydb["films"]
+    x = filmscol.insert_one({"name": "film5", "v_fname":"film5.mp4"})
+    print(x.inserted_id)
+    print(f'x is {x}')
+
+    return "", 200
+    # print(f'Request to add film. Name {data.name}, host {v_host}, fname {v_fname}')
+
+@dbapi.route("/films_in_db_temp/")
+def films_in_db_temp():
+    mongoclient = pymongo.MongoClient("mongodb://35.240.101.156:80")
+    mydb = mongoclient["filmsdb"]
+    filmscol = mydb["films"]
+    found = list(filmscol.find())
+
+    # from bson import json_util
+    for x in found:
+        print(f"found: {x}")
+        x.pop("_id")
+    return jsonify(found)
+    # return Response(json_util.dumps({"temp":found}), mimetype = 'application/json')
